cd3d112b04b716b03d4c0c0dcec5c2fa3549d60f
castledragon <starlight1532@gmail.com>
2026-05-14 21:21:32 +0900

    환경설정 추가 및 App.js 일부 수정

--- respla/src/main/replhome/src/App.js
+++ respla/src/main/replhome/src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import AppPather from './components/AppPather/AppPather';
 
 
-// 0707일요일 프젝시작
+// 0707일요일 프젝시작 //
 
 function App() {
   return (
     <div className="App">
       <AppPather></AppPather>
-
     </div>
   );
 }
